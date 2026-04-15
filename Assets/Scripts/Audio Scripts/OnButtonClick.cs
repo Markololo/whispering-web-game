827b5030fe20e01613963c3a784b01a0121657af
Selihom Efrem Ogbe <efremselihom1@gmail.com>
2026-04-14 22:19:34 -0400

    Added Main Menu audio Files for opening scene and button clicks

---
+++ Assets/Scripts/Audio Scripts/OnButtonClick.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnButtonClick : MonoBehaviour
+{
+    public AudioClip audioClip;
+    //public MainMenuCanvas mainMenu;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound()
+    {
+        //if (mainMenu != null) //idk if we will actually need this since it will change the scene when we click a button
+        //{
+        //    //mainMenu.StopMusic();
+        //}
+        if (audioSource != null && audioClip != null)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadSceneByIndex(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
