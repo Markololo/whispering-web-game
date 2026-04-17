80487efefe7047cbf449272070235b4110209c10
ayakharchafi <143551995+ayakharchafi@users.noreply.github.com>
2026-04-16 21:17:59 -0400

    portal exit done

---
+++ Assets/Scripts/LevelExitTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExitTrigger : MonoBehaviour
+{
+    public string nextSceneName = "Level2";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
