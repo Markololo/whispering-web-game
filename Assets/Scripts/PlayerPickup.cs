d5ad7a9e0ee72cc063310cf51e64f5fc29f3469a
ayakharchafi <143551995+ayakharchafi@users.noreply.github.com>
2026-04-16 20:54:02 -0400

    refixed pickup axe, done pickup flash,done break box

--- Assets/Scripts/PlayerPickup.cs
+++ Assets/Scripts/PlayerPickup.cs
@@ -12,46 +12,54 @@ public class PlayerPickup : MonoBehaviour
     private GameObject heldItem;
     private bool isAttacking = false;
 
-    public void OnInteract(InputValue value)
+  public void OnInteract(InputValue value)
+{
+    Debug.Log("Interact pressed");
+
+    if (playerCamera == null)
     {
-        Debug.Log("Interact pressed");
+        Debug.LogWarning("Player Camera is not assigned in PlayerPickup.");
+        return;
+    }
 
-        if (playerCamera == null)
+   
+    if (heldItem != null)
+    {
+        PickupItem heldPickup = heldItem.GetComponent<PickupItem>();
+        if (heldPickup != null)
         {
-            Debug.LogWarning("Player Camera is not assigned in PlayerPickup.");
-            return;
+            Vector3 dropPos = playerCamera.transform.position 
+                            + playerCamera.transform.forward * 1.2f 
+                            + Vector3.up * 0.3f;
+
+            heldPickup.Drop(dropPos);
         }
 
-        Vector3 direction = playerCamera.transform.forward + Vector3.down * 0.5f;
-        Ray ray = new Ray(playerCamera.transform.position, direction.normalized);
-        RaycastHit hit;
+        heldItem = null;
+        return; 
+    }
 
-        if (Physics.SphereCast(ray, 0.3f, out hit, pickupRange))
+    
+    Vector3 direction = playerCamera.transform.forward + Vector3.down * 0.5f;
+    Ray ray = new Ray(playerCamera.transform.position, direction.normalized);
+    RaycastHit hit;
+
+    if (Physics.SphereCast(ray, 0.3f, out hit, pickupRange))
+    {
+        Debug.Log("Hit object: " + hit.collider.gameObject.name);
+
+        PickupItem pickup = hit.collider.GetComponentInParent<PickupItem>();
+        if (pickup != null)
         {
-            Debug.Log("Hit object: " + hit.collider.gameObject.name);
-
-            PickupItem pickup = hit.collider.GetComponentInParent<PickupItem>();
-            if (pickup != null)
-            {
-                if (heldItem != null)
-                {
-                    PickupItem oldItem = heldItem.GetComponent<PickupItem>();
-                    if (oldItem != null)
-                    {
-                        Vector3 dropPos = playerCamera.transform.position + playerCamera.transform.forward * 1.2f;
-                        oldItem.Drop(dropPos);
-                    }
-                }
-
-                heldItem = pickup.PickUp(holdPoint);
-                Debug.Log("Picked up: " + pickup.itemName);
-            }
-        }
-        else
-        {
-            Debug.Log("Nothing hit");
+            heldItem = pickup.PickUp(holdPoint);
+            Debug.Log("Picked up: " + pickup.itemName);
         }
     }
+    else
+    {
+        Debug.Log("Nothing hit");
+    }
+}
 
     public void OnAttack(InputValue value)
     {
@@ -75,7 +83,7 @@ public class PlayerPickup : MonoBehaviour
         Quaternion startRot = heldItem.transform.localRotation;
 
         Vector3 attackPos = startPos + new Vector3(0.2f, -0.2f, 0.2f);
-        Quaternion attackRot = startRot * Quaternion.Euler(0f, 0f, -70f);
+      Quaternion attackRot = startRot * Quaternion.Euler(-70f, 0f, 0f);
 
         float time = 0f;
         float duration = 0.12f;
