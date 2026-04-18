5702d43853cd125f3918c386513ea58c8c8da707
Markololo <markololo2468@gmail.com>
2026-04-18 00:15:08 -0400

    added UI instructions for first playable, it'll be improved later

--- Assets/Scripts/State/IdleState.cs
+++ Assets/Scripts/State/IdleState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class IdlseState : IState
 {
     private SpiderController spiderController;
-    private float idleDuration = 120f;
+    private float idleDuration = 60f;
     private float idleTimer;
 
     public StateType Type => StateType.Idle;
