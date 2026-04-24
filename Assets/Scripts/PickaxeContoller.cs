044859610fadde358389e62ecef84dfcb26e1754
JonathanMarkovic <jonathan.markovic@outlook.com>
2026-04-24 09:21:14 -0400

    Updates
    Removed reduntant PickaxeController
    
    Updated Player prefab
    Make changes to pickaxe controller to swing on command

--- Assets/Scripts/PickaxeContoller.cs
+++ Assets/Scripts/PickaxeContoller.cs
@@ -1,19 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickaxeContoller : MonoBehaviour
 {
     private bool isActive;
-    
+    private bool isSwinging;
+    private Animator animator;
+    private Collider trigger;
+    public AudioClip axeHit;
+    private AudioSource source;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        trigger = GetComponent<Collider>();
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("isSwinging", true);
+            trigger.isTrigger = true;
+            source.clip = axeHit;
+            source.PlayOneShot(axeHit);
+        }
+        else
+        {
+            animator.SetBool("isSwinging", false);
+            trigger.isTrigger = false;
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Breakable")
+        {
+
+        }
     }
 }
